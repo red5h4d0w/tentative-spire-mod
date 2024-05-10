10cbab8d7114ec43a749f5fb5a672e07066e3ba8
red5h4d0w <45214944+red5h4d0w@users.noreply.github.com>
2024-05-10 19:27:29 -0400

    Create DracoFangRelic.java

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/relics/DracoFangRelic.java
@@ -0,0 +1,66 @@
+package mysteryDungeon.relics;
+
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.abstracts.PokemonRelic;
+import mysteryDungeon.pokemons.Charmander;
+import mysteryDungeon.util.TextureLoader;
+
+import static mysteryDungeon.MysteryDungeon.makeRelicOutlinePath;
+import static mysteryDungeon.MysteryDungeon.makeRelicPath;
+
+import java.util.HashSet;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.actions.unique.ExhumeAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.AbstractCard.CardColor;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.RelicStrings;
+
+public class DracoFangRelic extends PokemonRelic {
+
+    // ID, images, text.
+    public static final String ID = MysteryDungeon.makeID(DracoFangRelic.class);
+
+    private static final RelicStrings relicStrings = CardCrawlGame.languagePack.getRelicStrings(ID);
+    public static final String NAME = relicStrings.NAME;
+    public static final String[] DESCRIPTIONS = relicStrings.DESCRIPTIONS;
+    private static final Texture IMG = TextureLoader.getTexture(makeRelicPath("draco-fang.png"));
+    private static final Texture OUTLINE = TextureLoader.getTexture(makeRelicOutlinePath("draco-fang.png"));
+
+    public DracoFangRelic() {
+        super(ID, IMG, OUTLINE, RelicTier.UNCOMMON, LandingSound.CLINK);
+        cardColors = new HashSet<CardColor>() {
+            {
+                add(Charmander.CARD_COLOR);
+            }
+        };
+        counter = 0;
+    }
+
+    @Override
+    public void onExhaust(AbstractCard card) {
+        super.onExhaust(card);
+        counter++; 
+        if(counter == 10) {
+            addToBot(new ExhumeAction(false));
+            counter = 0;
+            pulse = false;
+        }
+    }
+
+    @Override
+    public void atBattleStart() {
+        super.atBattleStart();
+        if (counter == 9) {
+            beginPulse();
+            pulse = true;
+        }
+    }
+
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0];
+    }
+
+}
