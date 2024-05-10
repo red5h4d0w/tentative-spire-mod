f9125363984ffae180602a96828f96f294d4f8d3
red5h4d0w <45214944+red5h4d0w@users.noreply.github.com>
2024-05-10 19:02:17 -0400

    rose incense in code

---
+++ mysteryDungeon/src/main/java/mysteryDungeon/relics/RoseIncenseRelic.java
@@ -0,0 +1,37 @@
+package mysteryDungeon.relics;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.RelicStrings;
+
+import mysteryDungeon.MysteryDungeon;
+import mysteryDungeon.abstracts.PokemonRelic;
+import mysteryDungeon.util.TextureLoader;
+
+import static mysteryDungeon.MysteryDungeon.makeRelicOutlinePath;
+import static mysteryDungeon.MysteryDungeon.makeRelicPath;
+
+public class RoseIncenseRelic extends PokemonRelic {
+    public static final String ID = MysteryDungeon.makeID(RoseIncenseRelic.class);
+
+    private static final RelicStrings relicStrings = CardCrawlGame.languagePack.getRelicStrings(ID);
+    public static final String NAME = relicStrings.NAME;
+    public static final String[] DESCRIPTIONS = relicStrings.DESCRIPTIONS;
+    private static final Texture IMG = TextureLoader.getTexture(makeRelicPath("rose-incense.png"));
+    private static final Texture OUTLINE = TextureLoader.getTexture(makeRelicOutlinePath("rose-incense.png"));
+
+    public RoseIncenseRelic() {
+        super(ID, IMG, OUTLINE, RelicTier.SHOP, LandingSound.CLINK);
+    }
+
+    @Override
+    public int onPlayerHeal(int healAmount) {
+        return 1 + super.onPlayerHeal(healAmount);
+    }
+
+    // Description
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0];
+    }
+}
